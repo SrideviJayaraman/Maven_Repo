3981295611762b017bd2b3da60d6558b3b57ef20
sriid <sriidevii4@gmail.com>
2023-03-17 22:23:57 +0530

    Commit

---
+++ maven/src/main/java/pkgcalculator/Calculator.java
@@ -0,0 +1,34 @@
+package pkgcalculator;
+
+public class Calculator {
+
+	
+	// member variables
+	public String varHi="Hello world";
+	
+	// member functions
+	public String getwishes() {
+		return varHi;
+	}
+	
+	public int addition(int a,int b) {
+		return a+b;
+	}
+	
+	public int Substraction(int a,int b) {
+		return a-b;
+	}
+	
+	public int Multiplication(int a,int b) {
+		return a*b;
+	}
+	
+	public float division(int a,int b) {
+		return a/b;
+	}
+	
+	
+	
+	
+
+}
